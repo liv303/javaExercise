5a6598754309c48ef19d20f19b5525811cf72b75
Lee siyun <livre303@gmail.com>
2023-05-02 10:10:38 +0900

    feat: week3-day2 -01 - Factorial 기능 추가

---
+++ src/main/java/week3/day2/Factorial.java
@@ -0,0 +1,17 @@
+package week3.day2;
+
+import java.util.Scanner;
+
+public class Factorial {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int answer = 1; // 곱하기 연산은 0부터 시작할 수 없음
+
+
+        for (int i = n; i >= 1; i--) {
+            answer *= i;
+        }
+        System.out.println(answer);
+    }
+}
