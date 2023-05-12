46e10877e30b4068f7e2e98947645ffd1c30d7be
Lee siyun <livre303@gmail.com>
2023-05-12 19:19:10 +0900

    feat: week4 day1 -03

---
+++ src/main/java/week4/day1/Method/GreetTest.java
@@ -0,0 +1,8 @@
+package week4.day1.Method;
+
+public class GreetTest {
+    public static void main(String[] args) {
+        Greet greet = new Greet();  // 인스턴스 생성(HEAP에 올림)
+        greet.printHello(); // 호출
+    }
+}
